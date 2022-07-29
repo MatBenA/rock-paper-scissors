a242abdb128ad8a696f2f9a57e2abe21d4415dfe
MatiasB <crosserain@gmail.com>
2022-07-29 18:36:43 -0300

    Computer choice function finished

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
-prompt("hola");
-console.log("buenas tardes");
+/*
+a function called getComputerChoice that will randomly return
+either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function
+in the game to make the computer’s play.
+
+create a variable for the result
+
+randomly return rock, paper or scissors
+    randomly choose betweeen 1 and 3 inclusive
+
+store the value in result
+
+return the result
+
+*/
+
+function getComputerChoice() {
+
+    let result;
+
+    result = Math.floor(Math.random() * (3 - 1 + 1)) + 1; //randomly choose betweeen 1 and 3 inclusive
+
+    switch (result) {
+        case 1:
+            result = "Rock";
+            break;
+
+        case 2:
+            result = "Paper";
+            break;
+
+        case 3:
+            result = "Scissors";
+            break;
+
+        default:
+            console.log("error");
+    }
+
+    return result;
+}
+
+console.log(getComputerChoice());
